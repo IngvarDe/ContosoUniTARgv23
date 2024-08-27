30501acdb093b014ab86eb545e7a28925e3582cc
Ingvar Derešivski <inde999@gmail.com>
2024-08-27 16:13:21 +0300

    CreateDB

--- ContosoUniTARgv23/ContosoUniTARgv23/Program.cs
+++ ContosoUniTARgv23/ContosoUniTARgv23/Program.cs
@@ -38,9 +38,25 @@ namespace ContosoUniTARgv23
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
-
             app.Run();
+
+            static void CreateDbIfNotExists(IHost host)
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    try
+                    {
+                        var context = services.GetRequiredService<SchoolContext>();
+                        DbInitializer.Intialize(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "An error occured creating the DB.");
+                    }
+                }
+            }
         }
     }
 }
